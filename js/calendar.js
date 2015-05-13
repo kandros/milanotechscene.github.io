a2929f289553e88dd783561b5d9216b7aa4ad33c
Daniele Dellafiore <ildella@gmail.com>
2015-05-13 16:19:49 +0200

    added KO  ViewModel for event list. still getting a 401 from API..

--- js/calendar.js
+++ js/calendar.js
@@ -1,18 +1,31 @@
-function Task(data) {
+function Event(data) {
     this.title = ko.observable(data.name);
 }
 
-function TaskListViewModel() {
+function EventListViewModel() {
     // Data
     var self = this;
-    self.tasks = ko.observableArray([]);
+    self.events = ko.observableArray([]);
+  
+    // $.ajax({
+    //   type: "GET",
+    //   url: "https://milanotechscene.apispark.net/v1/companies",
+    //   dataType: 'json',
+    //   data: '{}',
+    //   username: "71b37fd5-8901-4ce3-8304-766a730aa39c",
+    //   password: "40c3a9d7-ed40-4767-88c2-1a0054e4a0b2",
+    //   success: function (){
+    //       alert('Thanks for your comment!'); 
+    //   }
+    // });
 
-    // Load initial state from server, convert it to Task instances, then populate self.tasks
     $.getJSON("https://milanotechscene.apispark.net/v1/companies", function(allData) {
-        var mappedTasks = $.map(allData, function(item) { return new Task(item) });
-        self.tasks(mappedTasks);
-    });      
+        var mappedEvents = $.map(allData, function(item) { return new Event(item) });
+        self.events(mappedEvents);
+    });
+
+
 //71b37fd5-8901-4ce3-8304-766a730aa39c:40c3a9d7-ed40-4767-88c2-1a0054e4a0b2
 }
 
-ko.applyBindings(new TaskListViewModel());
+ko.applyBindings(new EventListViewModel());
