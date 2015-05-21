47f1c6999b455d95e6ed3dc51a804f818e46ebd6
Daniele Dellafiore <ildella@gmail.com>
2015-05-21 17:34:40 +0200

    contact form integrated with intercom using KO binding

--- js/groups.js
+++ js/groups.js
@@ -3,9 +3,21 @@ function Group(data) {
     this.logo = ko.observable(data.logo);
 }
 
+function Event(data) {
+    this.summary = ko.observable(data.summary);
+    this.date = ko.observable(data.start.dateTime);
+    this.description = ko.observable(data.description);
+}
+
 function GroupListViewModel() {
     var self = this;
     self.groups = ko.observableArray([]);
+    self.email = ko.observable();
+    self.firstName = ko.observable();
+    self.lastName = ko.observable();
+    self.name = ko.computed(function() {
+        return this.firstName() + " " + this.lastName();    
+    }, this);
   
     $.getJSON("https://milanotechscene.apispark.net/v1/groups", function(allData) {
         var mappedGroups = $.map(allData, function(item) { return new Group(item) });
@@ -15,17 +27,20 @@ function GroupListViewModel() {
     self.events = ko.observableArray([]);
   
     $.getJSON("https://www.googleapis.com/calendar/v3/calendars/startmiup.it_lnvisvcuum6rdsm1v101fem2i8@group.calendar.google.com/events?key=AIzaSyBP-eVlQhmfD0Ml630CHwWxsv27k14zjfc", function(allData) {
-        console.log(allData.items)
         var mappedEvents = $.map(allData.items, function(item) { return new Event(item) });
         self.events(mappedEvents);
     });
 
-}
+    self.save = function() { 
+        Intercom('boot', {
+            app_id: "h5m9iozj",
+            email: self.email(),
+            firstname: self.firstName(),   
+            lastname: self.lastName(),
+            name: self.name()
+        });
+    };
 
-function Event(data) {
-    this.summary = ko.observable(data.summary);
-    this.date = ko.observable(data.start.dateTime);
-    this.description = ko.observable(data.description);
 }
 
 ko.applyBindings(new GroupListViewModel());
