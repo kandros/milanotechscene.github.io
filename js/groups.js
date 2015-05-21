616010d3e022a017ca26dab353f9dc0981ed2212
Daniele Dellafiore <ildella@gmail.com>
2015-05-21 17:47:07 +0200

    re-inserted the last element that links to complete calendar

--- js/groups.js
+++ js/groups.js
@@ -26,7 +26,7 @@ function GroupListViewModel() {
 
     self.events = ko.observableArray([]);
   
-    $.getJSON("https://www.googleapis.com/calendar/v3/calendars/startmiup.it_lnvisvcuum6rdsm1v101fem2i8@group.calendar.google.com/events?key=AIzaSyBP-eVlQhmfD0Ml630CHwWxsv27k14zjfc", function(allData) {
+    $.getJSON("https://www.googleapis.com/calendar/v3/calendars/startmiup.it_lnvisvcuum6rdsm1v101fem2i8@group.calendar.google.com/events?key=AIzaSyBP-eVlQhmfD0Ml630CHwWxsv27k14zjfc&maxResults=5", function(allData) {
         var mappedEvents = $.map(allData.items, function(item) { return new Event(item) });
         self.events(mappedEvents);
     });
