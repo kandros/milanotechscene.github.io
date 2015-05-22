12f6cb0919f4c109b32a56696f46c6d6b36413eb
Maurizio Mangione <granzebru@gmail.com>
2015-05-22 12:18:40 +0200

    bind url to UG logos

--- js/groups.js
+++ js/groups.js
@@ -1,6 +1,7 @@
 function Group(data) {
     this.name = ko.observable(data.name);
     this.logo = ko.observable(data.logo);
+    this.url = ko.observable(data.url);
 }
 
 function Event(data) {
@@ -16,26 +17,26 @@ function GroupListViewModel() {
     self.firstName = ko.observable();
     self.lastName = ko.observable();
     self.name = ko.computed(function() {
-        return this.firstName() + " " + this.lastName();    
+        return this.firstName() + " " + this.lastName();
     }, this);
-  
+
     $.getJSON("https://milanotechscene.apispark.net/v1/groups", function(allData) {
         var mappedGroups = $.map(allData, function(item) { return new Group(item) });
         self.groups(mappedGroups);
     });
 
     self.events = ko.observableArray([]);
-  
+
     $.getJSON("https://www.googleapis.com/calendar/v3/calendars/startmiup.it_lnvisvcuum6rdsm1v101fem2i8@group.calendar.google.com/events?key=AIzaSyBP-eVlQhmfD0Ml630CHwWxsv27k14zjfc&maxResults=5", function(allData) {
         var mappedEvents = $.map(allData.items, function(item) { return new Event(item) });
         self.events(mappedEvents);
     });
 
-    self.save = function() { 
+    self.save = function() {
         Intercom('boot', {
             app_id: "h5m9iozj",
             email: self.email(),
-            firstname: self.firstName(),   
+            firstname: self.firstName(),
             lastname: self.lastName(),
             name: self.name()
         });
