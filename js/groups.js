775d0e6dd467f113bb4a5d285a8aaae6152c0bcf
Daniele Dellafiore <ildella@gmail.com>
2015-05-24 16:55:30 +0200

    fix alla buona di 1, 2 e 4 di #5

--- js/groups.js
+++ js/groups.js
@@ -2,13 +2,14 @@ function Group(data) {
     this.name = ko.observable(data.name);
     this.logo = ko.observable(data.logo);
     this.url = ko.observable(data.url);
-    this.topics = ko.observable(data.topics)
+    this.topics = ko.observable(data.topics);
 }
 
 function Event(data) {
     this.summary = ko.observable(data.summary);
     this.date = ko.observable(data.start.dateTime);
     this.description = ko.observable(data.description);
+    this.htmlLink = ko.observable(data.htmlLink)
 }
 
 function GroupListViewModel() {
