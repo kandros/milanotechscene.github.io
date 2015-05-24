13c88f274b59feae18393124405196e75d474120
Daniele Dellafiore <ildella@gmail.com>
2015-05-24 15:33:44 +0200

    updated GCal query. Start time hard-coded but working, removed duplicated repeted events and fixed order by start time

--- js/groups.js
+++ js/groups.js
@@ -28,7 +28,7 @@ function GroupListViewModel() {
 
     self.events = ko.observableArray([]);
 
-    $.getJSON("https://www.googleapis.com/calendar/v3/calendars/startmiup.it_lnvisvcuum6rdsm1v101fem2i8@group.calendar.google.com/events?key=AIzaSyBP-eVlQhmfD0Ml630CHwWxsv27k14zjfc&maxResults=5", function(allData) {
+    $.getJSON("https://www.googleapis.com/calendar/v3/calendars/startmiup.it_lnvisvcuum6rdsm1v101fem2i8@group.calendar.google.com/events?key=AIzaSyBP-eVlQhmfD0Ml630CHwWxsv27k14zjfc&maxResults=5&singleEvents=true&orderBy=startTime&timeMin=2015-05-24T00%3A00%3A00%2B00%3A00", function(allData) {
         var mappedEvents = $.map(allData.items, function(item) { return new Event(item) });
         self.events(mappedEvents);
     });
